1737278738a8042868048fdc5b3955865408f04b
Ehsan Norouzi <ehsannorouzi@protonmail.com>
2024-04-12 20:59:42 +0330

    Feat: Product add image model and connect to product model

--- coffeeshop/product/models.py
+++ coffeeshop/product/models.py
@@ -3,11 +3,19 @@ from coffeeshop.common.models import BaseModel
 
 
 # Create your models here.
+class Image(BaseModel):
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.image.url
+
+
 class Product(BaseModel):
     name = models.CharField(max_length=100)
     detail = models.CharField(max_length=1000)
     price = models.FloatField()
     ingredients = models.CharField(max_length=1000)
+    images = models.ManyToManyField(Image)
 
     def __str__(self):
         return self.name
