5527e8a43e6ec2cbfe5f1f9c004e3798a777a587
Ehsan Norouzi <ehsannorouzi@protonmail.com>
2024-04-12 20:34:01 +0330

    Init: Initialize

---
+++ coffeeshop/api/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+
+urlpatterns = [
+    # path('blog/', include(('coffeeshop.blog.urls', 'blog')))
+]
---
+++ config/settings/swagger.py
@@ -0,0 +1,6 @@
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'CoffeeShop API',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
+
