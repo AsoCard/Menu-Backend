18e3367926ae3b53dc7e110d330629424da78858
Ehsan Norouzi <ehsannorouzi@protonmail.com>
2024-04-12 21:27:51 +0330

    Feat: Menu model

--- coffeeshop/menu/models.py
+++ coffeeshop/menu/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from coffeeshop.common.models import BaseModel
+
 
 # Create your models here.
+
+class Menu(BaseModel):
+    name = models.CharField(max_length=100)
+    products = models.ManyToManyField('product.Product')
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Menu"
+        verbose_name_plural = "Menus"
