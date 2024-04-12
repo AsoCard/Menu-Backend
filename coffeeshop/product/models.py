3c05551fd2fd79f7b9a90793a1f8a93d3fb0a26f
Ehsan Norouzi <ehsannorouzi@protonmail.com>
2024-04-12 20:57:02 +0330

    Feat: Product base model

--- coffeeshop/product/models.py
+++ coffeeshop/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from coffeeshop.common.models import BaseModel
+
 
 # Create your models here.
+class Product(BaseModel):
+    name = models.CharField(max_length=100)
+    detail = models.CharField(max_length=1000)
+    price = models.FloatField()
+    ingredients = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
