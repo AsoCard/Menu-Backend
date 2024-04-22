196de4a29606a4fdb2fbedd3a1921abb7df7163e
Ehsan Norouzi <ehsannorouzi@protonmail.com>
2024-04-22 21:22:32 +0330

    Feat: order model

--- coffeeshop/order/models.py
+++ coffeeshop/order/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from coffeeshop.common.models import BaseModel
+from coffeeshop.product.models import Product
+from coffeeshop.users.models import BaseUser
 # Create your models here.
+
+
+class Order(BaseModel):
+    STATUS_CHOICES = (
+        ('1', 'ثبت شده'),
+        ('2', 'در حال آماده سازی'),
+        ('3', 'آماده'),
+        ('4', 'تحویل داده شده'),
+    )
+    address = models.CharField(
+        max_length=1000
+    )
+    user = models.ForeignKey(
+        BaseUser, on_delete=models.CASCADE
+    )
+    orders = models.ManyToManyField(
+        to=Product
+    )
+    status = models.CharField(
+        choices=STATUS_CHOICES
+    )
+    des = models.TextField()
